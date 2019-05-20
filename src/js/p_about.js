59d1fdbc5d72cf0b49c7ddc24b1a51bcc341ef15
IAluI <29124424+ialui@users.noreply.github.com>
2019-05-20 14:18:55 +0700

    Небольшие изменения

--- src/js/p_about.js
+++ src/js/p_about.js
@@ -1,5 +1,40 @@
+'use strict';
+
 $(document).ready(function() {
   $('.Callback-Button').click(function () {
     console.log('click!')
-  })
+  });
+
+  $('.Dealers-Slider').slick({
+    slidesToShow: 3,
+    prevArrow: $('.Dealers-SliderArrow:first-child > img'),
+    nextArrow: $('.Dealers-SliderArrow:last-child > img'),
+    dots: true,
+    autoplay: false,
+    mobileFirst: true,
+    responsive: [{
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 6
+      }
+    }, {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 5
+      }
+    }, {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 4
+      }
+    }
+    ]
+  });
+  $('.Dealers-SliderArrow > img').click(function (e) {
+    $(e.target).css('transform', 'scale(1.3)');
+    setTimeout(function () {
+      $(e.target).css('transform', '');
+    }, 300);
+  });
+
 });
