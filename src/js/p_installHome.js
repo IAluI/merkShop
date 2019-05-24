ffd15037c6e65946229307e52bb8608ac9b6725d
IAluI <29124424+ialui@users.noreply.github.com>
2019-05-25 01:34:07 +0700

    Небольшие исправления

--- src/js/p_installHome.js
+++ src/js/p_installHome.js
@@ -1,6 +1,15 @@
 'use strict';
 
 $(document).ready(function() {
+  $('a[href^="#"]').click(function (e) {
+    e.preventDefault();
+    var id = $(this).attr('href');
+    var top = $(id).offset().top;
+    $('html, body').animate({
+      scrollTop: top
+    }, 350);
+  });
+
   $('.StdInstall-CaseImg').slick({
     slidesToShow: 1,
     dots: true,
@@ -22,12 +31,23 @@ $(document).ready(function() {
     ]
   });
 
-  $('a[href^="#"]').click(function (e) {
-    e.preventDefault();
-    var id = $(this).attr('href');
-    var top = $(id).offset().top;
-    $('html, body').animate({
-      scrollTop: top
-    }, 350);
+  $('.UnStdInstall-Cases').slick({
+    slidesToShow: 1,
+    dots: true,
+    arrows: false,
+    mobileFirst: true,
+    responsive: [
+      {
+        breakpoint: 575,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: 'unslick'
+      }
+    ]
   });
+
 });
