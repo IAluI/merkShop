c1f1d95779b92ea09412f48454d6355a90a06a9f
IAluI <29124424+ialui@users.noreply.github.com>
2019-05-28 14:18:33 +0700

    Небольшие изменения

--- src/js/p_installHome.js
+++ src/js/p_installHome.js
@@ -31,23 +31,44 @@ $(document).ready(function() {
     ]
   });
 
-  $('.UnStdInstall-Cases').slick({
-    slidesToShow: 1,
-    dots: true,
-    arrows: false,
-    mobileFirst: true,
-    responsive: [
-      {
-        breakpoint: 575,
-        settings: {
-          slidesToShow: 2
-        }
-      },
-      {
-        breakpoint: 768,
-        settings: 'unslick'
+  (function unStdInstallSlider() {
+    var root = $('.UnStdInstall-Cases');
+    var isDestroyed = false;
+    var media = window.matchMedia('(max-width: 767px)');
+    function slickInit() {
+      root.slick({
+        slidesToShow: 1,
+        dots: true,
+        arrows: false,
+        mobileFirst: true,
+        responsive: [
+          {
+            breakpoint: 575,
+            settings: {
+              slidesToShow: 2
+            }
+          },
+          {
+            breakpoint: 767,
+            settings: 'unslick'
+          }
+        ]
+      }, true);
+      console.log(slickInit)
+    };
+
+    root.on('destroy', function(){
+      isDestroyed = true;
+    });
+    slickInit();
+    $(window).resize($.debounce(250, function () {
+      console.log(isDestroyed);
+      console.log(media.matches);
+      if (media.matches && isDestroyed) {
+        slickInit();
+        isDestroyed = false;
       }
-    ]
-  });
+    }));
+  })();
 
 });
