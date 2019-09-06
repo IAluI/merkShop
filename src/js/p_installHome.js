983c451ccdf34130c1fa8eed555f3c1683f4739a
IAluI <29124424+ialui@users.noreply.github.com>
2019-09-06 12:46:54 +0700

    -Обновлены картинки -Добавлен слайдер "Этапы монтажа" на странице "Монтаж бытовых систем кондиционирования" -Небольшие изменения на страницах "Монтаж бытовых систем кондиционирования" и "Проектирование"

--- src/js/p_installHome.js
+++ src/js/p_installHome.js
@@ -10,71 +10,6 @@ $(document).ready(function() {
     }, 350);
   });
 
-  /*var installStepsSlider = new Swiper ('.InstallSteps-Slider', {
-    grabCursor: true,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    slidesPerView: 3,
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-  });*/
-
-  var installStepsSlider = swiperInit({
-    mediaQ: '(min-width: 1230px)',
-    swiperContainer: '.InstallSteps-Slider',
-    swiperOptions: {
-      grabCursor: true,
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-      slidesPerView: 1,
-      breakpointsInverse: true,
-      breakpoints: {
-        576: {
-          slidesPerView: 2,
-        },
-        768: {
-          slidesPerView: 3,
-        },
-        992: {
-          slidesPerView: 4,
-        }
-      },
-    }
-  });
-
-  var unstdInstallSlider = swiperInit({
-    mediaQ: '(min-width: 1230px)',
-    swiperContainer: '.UnstdInstall-Slider',
-    swiperOptions: {
-      grabCursor: true,
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-      slidesPerView: 1,
-      breakpointsInverse: true,
-      breakpoints: {
-        768: {
-          slidesPerView: 2,
-        },
-      },
-    }
-  });
-
   var stdInstallSlider1 = swiperInit({
     mediaQ: '(min-width: 1230px)',
     swiperContainer: '.StdInstall-Variant1',
@@ -144,6 +79,82 @@ $(document).ready(function() {
         },
       },
       observer: true,
+    }
+  });
+
+  var installStepsSlider = swiperInit({
+    mediaQ: '(min-width: 1230px)',
+    swiperContainer: '.InstallSteps-Slider',
+    swiperOptions: {
+      grabCursor: true,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      slidesPerView: 1,
+      breakpointsInverse: true,
+      breakpoints: {
+        576: {
+          slidesPerView: 2,
+        },
+        768: {
+          slidesPerView: 3,
+        },
+        992: {
+          slidesPerView: 4,
+        }
+      },
+    }
+  });
+
+  var unstdInstallSlider = swiperInit({
+    mediaQ: '(min-width: 1230px)',
+    swiperContainer: '.UnstdInstall-Slider',
+    swiperOptions: {
+      grabCursor: true,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      slidesPerView: 1,
+      breakpointsInverse: true,
+      breakpoints: {
+        768: {
+          slidesPerView: 2,
+        },
+      },
+    }
+  });
+
+  var unstdInstallSlider = swiperInit({
+    mediaQ: '(min-width: 992px)',
+    swiperContainer: '.TwoStepsInstall-Slider',
+    swiperOptions: {
+      grabCursor: true,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      slidesPerView: 1,
+      //freeMode: true,
+      breakpointsInverse: true,
+      breakpoints: {
+        576: {
+          slidesPerView: 2,
+        },
+      },
     }
   });
 });
